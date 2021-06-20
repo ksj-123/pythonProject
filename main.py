b60fc21208aec825f3c587e5baf2b02b67a2d9ba
Kissne Simko Judit <kissnesimkojudit@gmail.com>
2021-06-20 22:19:31 +0200

    main.py modositasok

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
+student_1 = "Tamas"
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+print("hello, {}",format(student_1))
 
+student_2 = "Agi"
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("hello, {}", format(student_2))
---
+++ n_main.py
@@ -0,0 +1,9 @@
+x = 0
+
+for i in range(3):
+    my_input = input()
+    x += int(my_input)
+
+print(x)
+
+
