e237e2d091419617d3a85fe63852650a56f06286
Kissne Simko Judit <kissnesimkojudit@gmail.com>
2021-06-22 00:27:05 +0200

    main.py modositasok

---
+++ adat.py
@@ -0,0 +1,8 @@
+print('A')  # str
+print(12)   # int
+print(0.5)  #float
+print(True, False)  # bool
+
+alma = 3.3
+print(type(alma))
+print(int(alma))
---
+++ first_modul.py
@@ -0,0 +1,4 @@
+variable = 1;
+
+def fn(a,b):
+    return a + b
