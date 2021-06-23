c7f8e8a562ab0603e37861e8be30186753a3efc5
Kissne Simko Judit <kissnesimkojudit@gmail.com>
2021-06-23 22:59:28 +0200

    modositasok

--- kitchensink.py
+++ kitchensink.py
@@ -4,12 +4,15 @@ driver = webdriver.Chrome()
 try:
     driver.get("https://localhost:9999/trickybuttons.html")
 
-    button2 = driver.find_element_by_xpath('//button[text() = "button2"]')
-    button2.click()
+    buttons = driver.find_element_by_xpath('//button')
+    print(buttons)
+    print(type(buttons))
 
-    result = driver.find_element_by_xpath('//label[@id="result"]')
-    print(result.text)
+    for button in buttons:
+        button.click()
+        result = driver.find_element_by_xpath('//label[@id="result"]')
+        print(result.text)
 
-    assert (result.text == "button2 was clicked")
+        assert (result.text == f"{button.text} was clicked")
 finally:
     driver.close()
