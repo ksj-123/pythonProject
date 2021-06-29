a34fab31a759a557fc931720702a028bdb1930ba
Kissne Simko Judit <kissnesimkojudit@gmail.com>
2021-06-29 19:51:14 +0200

    modositasok

--- csop2.py
+++ csop2.py
@@ -1,4 +1,10 @@
 from selenium import webdriver
 
 driver = webdriver.Chrome()
-driver.get("http://localhost:9999/filltablewithsum.html")
+driver.get("https://react-card-2a6c5.web.app/")
+
+t_shirt = driver.find_elements_by_class_name("<div class="shelf-item__buy-btn">Add to cart</div>")
+print(t_shirt)
+
+for x in t_shirt:
+    x.click()
