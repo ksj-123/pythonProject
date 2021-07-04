15e4db292cff1137fd28025a5d0c9a11afa6500d
Kissne Simko Judit <kissnesimkojudit@gmail.com>
2021-07-04 19:13:53 +0200

    modositasok

--- ELOKESZITES/py_file_be_ki_olvasas.py
+++
@@ -1,5 +0,0 @@
-with open("petofi.txt", 'r', encoding="utf-8") as petofi:
-    with open("res.csv", 'w', encoding="utf-8") as res:
-        words = petofi.read().replace("\n", " ").replace(" ", "").split(",")
-        for word in words[::2]:
-            res.write(f"{words.index(word)}, {word.lower()}\n")
--- ELOKESZITES/py_fileolvkarakt.py
+++
@@ -1,5 +0,0 @@
-with open("input.txt", "r") as f:
-    res = f.read(5)
-    res2 = f.read(2)
-print(res)
-print(res2)
